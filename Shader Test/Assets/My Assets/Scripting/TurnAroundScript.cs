b4f2d5b2eab90762eb762d341e6f79c5b5a52438
mentalpop <43460243+mentalpop@users.noreply.github.com>
2019-08-26 15:06:42 -0400

    New Dialogue Updates
    - Fixed Bark UI
    - Added Barks to some Objects
    - Readded Fader
    - Fixed several Camera issues

--- Shader Test/Assets/My Assets/Scripting/TurnAroundScript.cs
+++ Shader Test/Assets/My Assets/Scripting/TurnAroundScript.cs
@@ -1,14 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class TurnAroundScript : MonoBehaviour
 {
 
 
-    public float turnSpeed = 40f;
+    public float turnSpeed = 20f;
     // Use this for initialization
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.Rotate(0, Time.deltaTime * turnSpeed, 0);
     }
