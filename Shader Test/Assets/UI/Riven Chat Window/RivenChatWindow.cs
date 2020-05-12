990f6b555dde53ff83875bb794287d96f92cc718
BluishGreenPro <bluish-green_productions@hotmail.com>
2020-05-12 16:58:04 -0400

    Layout fixes to Riven's Chat Window

---
+++ Shader Test/Assets/UI/Riven Chat Window/RivenChatWindow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RivenChatWindow : MonoBehaviour
+{
+    public ButtonGeneric closeButton;
+    public ClickToClose clickToClose;
+
+    private void OnEnable() {
+		clickToClose.OnClick += Close;
+		closeButton.OnClick += Close;
+	}
+
+	private void OnDisable() {
+        clickToClose.OnClick -= Close;
+		closeButton.OnClick -= Close;
+	}
+
+	private void Close() {
+		gameObject.SetActive(false);
+	}
+}
