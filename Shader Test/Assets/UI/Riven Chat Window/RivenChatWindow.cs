83cf4794719e4bc58cc896daa2e8df472f9b8a8e
BluishGreenPro <bluish-green_productions@hotmail.com>
2020-05-27 13:53:03 -0400

    Implemented Tony's Fix of Riven's Window

--- Shader Test/Assets/UI/Riven Chat Window/RivenChatWindow.cs
+++ Shader Test/Assets/UI/Riven Chat Window/RivenChatWindow.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using PixelCrushers.DialogueSystem;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,17 +8,66 @@ public class RivenChatWindow : MonoBehaviour
     public ButtonGeneric closeButton;
     public ClickToClose clickToClose;
 
-    private void OnEnable() {
-		clickToClose.OnClick += Close;
-		closeButton.OnClick += Close;
-	}
+    // Some of this code saves the chat text between uses so you can restore it 
+    // when the conversation starts again.
 
-	private void OnDisable() {
+    // Set this true if you want to restore the previous chat text when restarting
+    // the conversation:
+    public bool accumulateText = false;
+
+    // We'll save the old chat text in this string variable:
+    private string accumulatedText = string.Empty;
+
+    private StandardDialogueUI dialogueUI; // Handy reference to the chat dialogue UI.
+
+    private void Awake()
+    {
+        dialogueUI = GetComponent<StandardDialogueUI>();
+    }
+
+    private void OnEnable()
+    {
+        dialogueUI.conversationUIElements.mainPanel.onOpen.AddListener(ConversationStarted);
+        dialogueUI.conversationUIElements.mainPanel.onClose.AddListener(ConversationEnded);
+        clickToClose.OnClick += Close;
+        closeButton.OnClick += Close;
+    }
+
+    private void OnDisable()
+    {
+        dialogueUI.conversationUIElements.mainPanel.onOpen.RemoveListener(ConversationStarted);
+        dialogueUI.conversationUIElements.mainPanel.onClose.RemoveListener(ConversationEnded);
         clickToClose.OnClick -= Close;
-		closeButton.OnClick -= Close;
-	}
+        closeButton.OnClick -= Close;
+    }
 
-	private void Close() {
-		gameObject.SetActive(false);
-	}
+    private void ConversationStarted()
+    {
+        if (accumulateText)
+        {
+            // When conversation starts, restore the previously-accumulated text:
+            dialogueUI.conversationUIElements.defaultNPCSubtitlePanel.accumulatedText = accumulatedText;
+        }
+    }
+
+    private void ConversationEnded()
+    {
+        // When conversation ends, make sure this GameObject gets deactivated:
+        gameObject.SetActive(false);
+    }
+
+    private void Close()
+    {
+
+        if (accumulateText)
+        {
+            // When conversation ends, record the accumulated text:
+            accumulatedText = dialogueUI.conversationUIElements.defaultNPCSubtitlePanel.accumulatedText;
+        }
+
+        // When this GameObject is deactivated, make sure the conversation is stopped:
+        DialogueManager.StopConversation();
+
+        gameObject.SetActive(false);
+    }
 }
